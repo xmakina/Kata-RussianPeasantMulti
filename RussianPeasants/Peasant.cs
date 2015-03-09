18cc79bd1d3246a8071fb50228b44577be081e6e
Alex Winson <awinson@neworbit.co.uk>
2015-03-09 13:22:31 +0000

    Russian Peasants implemented

---
+++ RussianPeasants/Peasant.cs
@@ -0,0 +1,49 @@
+﻿namespace RussianPeasants
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Peasant
+    {
+        public PeasantResults Multiply(int left, int right)
+        {
+            // The Growths is the list of divisions/multiplications
+            var growths = new Dictionary<int, int> { { left, right } };
+
+            while (left != 1)
+            {
+                // Divide the left by two, ignore remainders
+                left = left / 2;
+                // Double the right
+                right = right * 2;
+
+                growths.Add(left, right);
+            }
+
+            // Store each growth pair that didn't have an even number on the left
+            var additions = growths.Where(growth => growth.Key % 2 != 0).ToDictionary(addition => addition.Key, addition => addition.Value);
+
+            // Add up all the addition values
+            var result = additions.Sum(addition => addition.Value);
+
+            // Return a class containing all our workings so we can see how the peasants reached their conclusions
+            return new PeasantResults(growths, additions, result);
+        }
+    }
+    
+    public class PeasantResults
+    {
+        public readonly Dictionary<int, int> Growths;
+
+        public readonly Dictionary<int, int> Additions;
+
+        public readonly int Result;
+
+        public PeasantResults(Dictionary<int, int> growths, Dictionary<int, int> additions, int result)
+        {
+            this.Growths = growths;
+            this.Additions = additions;
+            this.Result = result;
+        }
+    }
+}
---
+++ RussianPeasants/PeasantSpecs.cs
@@ -0,0 +1,45 @@
+﻿namespace RussianPeasants
+{
+    using System;
+
+    using Machine.Fakes;
+    using Machine.Specifications;
+
+    public abstract class WithPeasant : WithSubject<Peasant>
+    {
+
+    }
+
+    public class when_told_to_multiply_eighteen_by_twenty_three : WithPeasant
+    {
+        private static PeasantResults result;
+
+        private Because of = () => result = Subject.Multiply(18, 23);
+
+        private It should_have_the_correct_amount_of_growths = () => result.Growths.Count.ShouldEqual(5);
+
+        private It should_have_the_correct_amount_of_additions = () => result.Additions.Count.ShouldEqual(2);
+
+        private It should_return_four_hundred_and_fourteen = () => result.Result.ShouldEqual(414);
+    }
+
+    public class when_given_random_numbers : WithPeasant
+    {
+        private It should_get_it_right_lots_of_times = () =>
+        {
+            var loop = 0;
+
+            var random = new Random();
+
+            while (loop < 40000)
+            {
+                int left = random.Next(1, 40000);
+                int right = random.Next(1, 40000);
+
+                Subject.Multiply(left, right).Result.ShouldEqual(left * right);
+                Console.WriteLine("{0} * {1} = {2}", left, right, left * right);
+                loop++;
+            }
+        };
+    }
+}
